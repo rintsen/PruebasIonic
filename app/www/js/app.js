b1d9bc92b3091c67b06e5257e28fce85062fe026
Isa <chavela.isa@gmail.com>
2015-10-04 19:56:40 -0430

    Commit con el header funcional

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,19 +1,44 @@
-// Ionic Starter App
+var app = angular.module('app', ['ionic','ui.router','ngCordova']);
+app.config(['$stateProvider','$urlRouterProvider',function ($stateProvider,$urlRouterProvider){
+  $urlRouterProvider.otherwise("/index");
+  $stateProvider
+    .state('index',{
+      abstract:true,
+      url:'/index',
+      template:'<ion-nav-view/>',
+      templateUrl:'index.html',
+      controller:'controllerIdx',
+    })
+    .state('list',{
+      url:'list',
+      templateUrl:'View/Jade/list.html'
+    })
+}])
+app.controller('controllerIdx',['$scope',function($scope){
+  $scope.todos = [];
+  $scope.nombre;
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+  $scope.add = function(name){
+    console.log(name);
+    $scope.nombre = name
+    $scope.todos.push($scope.nombre);
+    $scope.nombre = "";
+  }
 
-.run(function($ionicPlatform) {
+  $scope.viewList = function() {
+    $state.go("list");
+  }
+}])
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+    // // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // // for form inputs)
+    // if(window.cordova && window.cordova.plugins.Keyboard) {
+    //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    // }
+    // if(window.StatusBar) {
+    //   StatusBar.styleDefault();
+    // }
   });
-})
+});
+
